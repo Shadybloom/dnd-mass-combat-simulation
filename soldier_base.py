964b6d7f1330ffcb460b44bcbb2e7839d964a2d2
Shadybloom Applepie <celestia@safe-mail.net>
2020-05-30 09:03:39 +0300

    Дополнение. Feat_Tough

--- soldier_base.py
+++ soldier_base.py
@@ -671,6 +671,9 @@ class soldier():
         if hasattr(self, 'hitpoints_medial') and self.hitpoints_medial:
             hitpoints_levels = sum([(int(hit_dice.split('d')[1]) / 2 + 0.5) for x in range(level)])
             hitpoints_max = hitpoints_levels + bonus_hitpoints_constitution
+        elif hasattr(self, 'hitpoints_base') and self.hitpoints_base:
+            hitpoints_levels = self.hitpoints_base
+            hitpoints_max = hitpoints_levels + bonus_hitpoints_constitution
         else:
             # На первом уровне максимум хитов:
             level_1_hitpoints = int(hit_dice.split('d')[1])
@@ -678,6 +681,9 @@ class soldier():
             if level > 1:
                 bonus_hitpoints_levels = sum([dices.dice_throw(hit_dice) for x in range(level - 1)])
             hitpoints_max = level_1_hitpoints + bonus_hitpoints_levels + bonus_hitpoints_constitution
+        # "Крепкий" Feat_Tough получает +2 hp/уровень:
+        if self.class_features.get('Feat_Tough'):
+            hitpoints_max += level * 2
         return int(hitpoints_max)
 
     def takeoff_armor(self):
